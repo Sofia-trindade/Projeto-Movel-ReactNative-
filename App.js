6d48fe421f4364fa76824598106e660b36a90e40
sofia trindade <sofiaatrindadee02@gmail.com>
2024-03-04 14:32:55 -0300

    adiciona idade no componente Cat

--- App.js
+++ App.js
@@ -2,12 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 const getNomeCompleto = (nome, sobrenome) =>{
-  return nome + ' ' + sobrenome;
+  return nome + ' ' + sobrenome ;
 }
-
-const Cat = ({nome, sobrenome}) => {
+const Cat = ({nome, sobrenome, idade}) => {
   
-  return <Text>Hello, I am {getNomeCompleto(nome, sobrenome).toUpperCase()}</Text>
+  return <Text>Hello, I am {getNomeCompleto(nome, sobrenome).toUpperCase()}, {idade} year.</Text>
 }
 
 export default IFAL;
@@ -15,9 +14,9 @@ export default IFAL;
 function IFAL() {
   return (
     <View style={styles.container}>
-    <Cat nome="Black" sobrenome="write"/>
-      <Cat nome="Xaninha" sobrenome="ket"/>
-      <Cat nome="Oliver" sobrenome="broow"/>
+    <Cat nome="Black" sobrenome="write" idade={5}/>
+      <Cat nome="Xaninha" sobrenome="ket" idade={2}/>
+      <Cat nome="Oliver" sobrenome="broow" idade={9}/>
 
       <StatusBar style="auto" />
     </View>
